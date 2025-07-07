f85877d454be471f3303337e6f01b9e72e00360f
Telychko Vitalii <vtelychko@adnoc.ae>
2025-07-07 18:46:46 +0300

    sdf

--- src/app/auth/auth.tsx
+++ src/app/auth/auth.tsx
@@ -41,29 +41,31 @@ export default function Auth() {
 
   console.log("RENDER");
 
-  (window as any).onTelegramAuth = async (userData: IUser) => {
-    const response = await createUser(userData);
+  if (window) {
+    (window as any).onTelegramAuth = async (userData: IUser) => {
+      const response = await createUser(userData);
 
-    if (response.success) {
-      window.location.href = "/courses";
-    }
-  };
+      if (response.success) {
+        window.location.href = "/courses";
+      }
+    };
 
-  const scriptId = "telegram-login-script";
-  if (document.getElementById(scriptId)) return; // вже є
+    const scriptId = "telegram-login-script";
+    if (document.getElementById(scriptId)) return; // вже є
 
-  const script = document.createElement("script");
-  script.src = "https://telegram.org/js/telegram-widget.js?22";
-  script.setAttribute("data-telegram-login", "telychko_frontend_courses_bot"); // 👈 заміни на свого бота
-  script.setAttribute("data-size", "large");
-  script.setAttribute("data-userpic", "false");
-  script.setAttribute("data-request-access", "write");
-  script.setAttribute("data-onauth", "onTelegramAuth(user)"); // 👈 виклик глобальної функції
-  script.id = scriptId;
-  script.async = true;
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.setAttribute("data-telegram-login", "telychko_frontend_courses_bot"); // 👈 заміни на свого бота
+    script.setAttribute("data-size", "large");
+    script.setAttribute("data-userpic", "false");
+    script.setAttribute("data-request-access", "write");
+    script.setAttribute("data-onauth", "onTelegramAuth(user)"); // 👈 виклик глобальної функції
+    script.id = scriptId;
+    script.async = true;
 
-  console.log(document.getElementById("telegram-login-btn"), script);
-  document.getElementById("telegram-login-btn")?.appendChild(script);
+    console.log(document.getElementById("telegram-login-btn"), script);
+    document.getElementById("telegram-login-btn")?.appendChild(script);
+  }
 
   return <div id="telegram-login-btn" />;
 }
