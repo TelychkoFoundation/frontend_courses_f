3d268a9c6ff058f1353111edbf2189a07341b6f0
Telychko Vitalii <vtelychko@adnoc.ae>
2025-07-07 18:45:45 +0300

    sdf

--- src/app/auth/auth.tsx
+++ src/app/auth/auth.tsx
@@ -5,39 +5,65 @@ import { createUser } from "../lib/postActions";
 import { IUser } from "../models/User";
 
 export default function Auth() {
-  useEffect(() => {
-    console.log("Auth");
-    // Створюємо глобальну функцію, яку Telegram викличе
-    (window as any).onTelegramAuth = async (userData: IUser) => {
-      const response = await createUser(userData);
+  // useEffect(() => {
+  //   console.log("Auth");
+  //   // Створюємо глобальну функцію, яку Telegram викличе
+  //   (window as any).onTelegramAuth = async (userData: IUser) => {
+  //     const response = await createUser(userData);
+  //
+  //     if (response.success) {
+  //       window.location.href = "/courses";
+  //     }
+  //   };
+  //
+  //   const scriptId = "telegram-login-script";
+  //   if (document.getElementById(scriptId)) return; // вже є
+  //
+  //   const script = document.createElement("script");
+  //   script.src = "https://telegram.org/js/telegram-widget.js?22";
+  //   script.setAttribute("data-telegram-login", "telychko_frontend_courses_bot"); // 👈 заміни на свого бота
+  //   script.setAttribute("data-size", "large");
+  //   script.setAttribute("data-userpic", "false");
+  //   script.setAttribute("data-request-access", "write");
+  //   script.setAttribute("data-onauth", "onTelegramAuth(user)"); // 👈 виклик глобальної функції
+  //   script.id = scriptId;
+  //   script.async = true;
+  //
+  //   console.log(document.getElementById("telegram-login-btn"), script);
+  //   document.getElementById("telegram-login-btn")?.appendChild(script);
+  //
+  //   // Cleanup
+  //   return () => {
+  //     console.log("unmount");
+  //     document.getElementById(scriptId)?.remove();
+  //   };
+  // }, []);
 
-      if (response.success) {
-        window.location.href = "/courses";
-      }
-    };
+  console.log("RENDER");
 
-    const scriptId = "telegram-login-script";
-    if (document.getElementById(scriptId)) return; // вже є
+  (window as any).onTelegramAuth = async (userData: IUser) => {
+    const response = await createUser(userData);
 
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.setAttribute("data-telegram-login", "telychko_frontend_courses_bot"); // 👈 заміни на свого бота
-    script.setAttribute("data-size", "large");
-    script.setAttribute("data-userpic", "false");
-    script.setAttribute("data-request-access", "write");
-    script.setAttribute("data-onauth", "onTelegramAuth(user)"); // 👈 виклик глобальної функції
-    script.id = scriptId;
-    script.async = true;
+    if (response.success) {
+      window.location.href = "/courses";
+    }
+  };
 
-    console.log(document.getElementById("telegram-login-btn"), script);
-    document.getElementById("telegram-login-btn")?.appendChild(script);
+  const scriptId = "telegram-login-script";
+  if (document.getElementById(scriptId)) return; // вже є
 
-    // Cleanup
-    return () => {
-      console.log("unmount");
-      document.getElementById(scriptId)?.remove();
-    };
-  }, []);
+  const script = document.createElement("script");
+  script.src = "https://telegram.org/js/telegram-widget.js?22";
+  script.setAttribute("data-telegram-login", "telychko_frontend_courses_bot"); // 👈 заміни на свого бота
+  script.setAttribute("data-size", "large");
+  script.setAttribute("data-userpic", "false");
+  script.setAttribute("data-request-access", "write");
+  script.setAttribute("data-onauth", "onTelegramAuth(user)"); // 👈 виклик глобальної функції
+  script.id = scriptId;
+  script.async = true;
+
+  console.log(document.getElementById("telegram-login-btn"), script);
+  document.getElementById("telegram-login-btn")?.appendChild(script);
 
   return <div id="telegram-login-btn" />;
 }
