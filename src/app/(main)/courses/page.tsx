9670ade1ec9cfee0b7834af30a91375c328f14b0
Telychko Vitalii <vtelychko@adnoc.ae>
2025-07-12 01:17:33 +0300

    sdf

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,20 +1,32 @@
-import { ReactElement } from "react";
+"use client";
+
+import { ReactElement, useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { getAllAdminCourses } from "../../lib/getActions";
 import Course from "./course";
 import { ICourse } from "../../models/Course";
 import { CoursesHeader } from "./header";
 
-export default async function Page() {
-  const response = await getAllAdminCourses("asc");
+export default function Page() {
+  const [data, setData] = useState<ICourse[]>([]);
 
-  console.log(response, "response");
+  // const response = await getAllAdminCourses("asc");
+  //
+  // console.log(response, "response");
+  useEffect(() => {
+    const b = async () => {
+      const r = await getAllAdminCourses("asc");
+      setData(r.data);
+    };
+
+    b();
+  }, []);
 
   return (
     <>
       <CoursesHeader />
       <div className={styles.coursesContainer}>
-        {response?.data?.map(
+        {data.map(
           (course: ICourse): ReactElement => (
             <Course key={course.id} data={course} />
           ),
