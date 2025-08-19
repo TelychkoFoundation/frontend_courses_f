a17abcbaab1ec3657adf147195e6a4efe6556167
Telychko Vitalii <vtelychko@adnoc.ae>
2025-08-19 23:30:17 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,24 @@ import {
   About,
   Footer,
 } from "./landing";
+import { Suspense } from "react";
 import styles from "../app/landing/index.module.css";
 
 export default async function Page() {
   return (
-    <div className={styles.wrapper}>
-      <Hero />
-      <Tutorial />
-      <Cta />
-      <Services />
-      <About />
-      <Feedback />
-      <Courses />
-      <Prices />
-      <Contacts />
-      <Footer />
-    </div>
+    <Suspense>
+      <div className={styles.wrapper}>
+        <Hero />
+        <Tutorial />
+        <Cta />
+        <Services />
+        <About />
+        <Feedback />
+        <Courses />
+        <Prices />
+        <Contacts />
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
