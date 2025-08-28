67d6ca5b05f11046a55b3be0d2a0e93b200c1be4
Telychko Vitalii <vtelychko@adnoc.ae>
2025-08-29 00:25:18 +0300

    test

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components";
-import { NotFoundImage } from "@/images";
+import { DeviceTypes, useDeviceType } from "@/hooks";
+import { NotFoundImage, Sharp, Road, Four } from "@/images";
 import styles from "./not-found.module.css";
 
-export default async function NotFound() {
+export default function NotFound() {
+  const deviceType = useDeviceType();
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>
         Отакої! Ця сторінка втекла, спробуй повернутися на головну
       </h2>
-      <Link href="/courses" className="button">
+      <Link href="/courses">
         <Button>На головну</Button>
       </Link>
-      <Image src={NotFoundImage} alt="Not found image" />
+      <section className={styles.images}>
+        <Image src={NotFoundImage} alt="404" />
+        <Image src={Sharp} alt="Sharp" />
+        {deviceType === DeviceTypes.mobile && <Image src={Road} alt="Road" />}
+        <Image src={Four} alt="4" />
+      </section>
     </div>
   );
 }
