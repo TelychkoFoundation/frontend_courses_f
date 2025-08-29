8f095ce43384807f8fc0c822b32984e670ef78d7
Telychko Vitalii <vtelychko@adnoc.ae>
2025-08-29 10:44:40 +0300

    test

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -10,7 +10,6 @@ export const { handlers } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.AUTH_SECRET,
   callbacks: {
     async jwt({ token, profile }: any) {
       if (profile) {
