6e39c170a070e36755a39529daf292122b776e3c
Telychko Vitalii <vtelychko@adnoc.ae>
2025-09-03 10:02:03 +0300

    test

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { Suspense } from "react";
 import ProfileNavigation from "./ProfileNavigation";
 import ProfileContent from "./ProfileContent";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <div className={styles.profileContainer}>
-      <ProfileNavigation />
-      <ProfileContent />
-    </div>
+    <Suspense>
+      <div className={styles.profileContainer}>
+        <ProfileNavigation />
+        <ProfileContent />
+      </div>
+    </Suspense>
   );
 }
